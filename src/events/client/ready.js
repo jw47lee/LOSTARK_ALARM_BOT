59d132649955f27f07095775df34d7b947328cbf
jw47lee <dlwldnjs803@gmail.com>
2022-10-15 20:20:04 -0400

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,23 +1,101 @@
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
   name: "ready",
   once: true,
   async execute(interaction, client) {
     console.log(`Ready!!! ${client.user.tag} is logged in and online.`);
 
-    var refresh_time = 5000;
-    var foo = true
+    var refresh_time = 1000;
+    var test_channel = client.channels.cache.get("1030905589410304134");
+
+    test_channel.send(
+      "Hello! LostARK bot is online! I will let you know when Legendary rapport item is up!"
+    );
+
     setInterval(() => {
-        client.channel.sendmessage("here")
+      const time = new Date();
+      const current_minute = time.getMinutes();
 
+      // test input
+      /** 
+      console.log(current_minute);
+      var test_result = new Map([
+        ["Continent", ["Area", "Legendary_item_name"]],
+      ]);
+      console.log("Continent");
+      console.log(test_result.get("Continent"));
+      embed_message = embed_creator(
+        client,
+        "Continent",
+        test_result.get("Continent")
+      );
+      test_channel.send({
+        embeds: [embed_message],
+      });
+      */
 
-        // run crawler
-        // update the list
-        // if there is a change in the list, send embedded message
-        
-        
+      if (current_minute > 30 && current_minute < 55) {
+        console.log("do the crawler");
+        //run crawler and get result
+        // TODO
+        //result = crawler();
+        if (Object.keys(result).length != Object.keys(rapport_dic).length) {
+          for (const [key, value] of Object.entries(result)) {
+            if (!(key in rapport_dic)) {
+
+              // if there is a change in the list, send embedded message
+              // add rapport
+
+              rapport_dic[key] = value;
+              embed_message = embed_creator(key, value);
+              test_channel.send({
+                embeds: [embed_creator()],
+              });
+            }
+          }
+        }
+
+      } else {
+        rapport_dic = {};
+      }
     }, refresh_time);
   },
 };
 
+var rapport_list = {};
 
-var rapport_list = []
+function embed_creator(client, continent, info) {
+  /*
+    if key == 어느 장소
+        item_name = 어느 아이템
+        item_map = map_url
+    */
+
+  // example map
+  var item_map = "https://i.imgur.com/oqTzPl3.png";
+
+  const embed = new EmbedBuilder()
+    .setTitle("전호 @" + continent)
+    .setTimestamp(Date.now())
+    .setColor(0xfaa300)
+    .setAuthor({
+      iconURL: client.user.displayAvatarURL(),
+      name: client.user.tag,
+    })
+    .setFooter({
+      iconURL: client.user.displayAvatarURL(),
+      text: client.user.tag,
+    })
+    .setURL(item_map)
+    .setImage(item_map)
+    .addFields([
+      {
+        name: info[0],
+        value: info[1],
+        inline: true,
+      },
+    ]);
+
+  return embed;
+}
